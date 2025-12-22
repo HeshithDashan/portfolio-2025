5262a8ad9622796638a71f22b1e3938b9f851435
Heshith <heshithdashan24@gmail.com>
2025-12-22 19:17:10 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 import Hero from "../components/Hero";
 import Skills from "../components/Skills";
-import Projects from "../components/Projects"; // 1. අලුත් කෑල්ල මෙතනින් ගත්තා
+import Projects from "../components/Projects"; 
 
 export default function Home() {
   return (
-    <main className="min-h-screen pb-20"> {/* 2. යටින් පොඩි ඉඩක් තිබ්බා (pb-20) */}
+    <main className="min-h-screen pb-20"> 
        <Hero />
        
        <Skills /> 
 
-       {/* 3. Projects එක මෙතනට දැම්මා */}
        <Projects /> 
     </main>
   );
